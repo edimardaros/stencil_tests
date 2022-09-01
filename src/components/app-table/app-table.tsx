781f4f41124f18bf817116c73c60a9d4d28899fd
Edimar Nascimento <edimar.daros@gmail.com>
2022-09-01 17:18:49 -0300

    Feat: Estilos

--- src/components/app-table/app-table.tsx
+++ src/components/app-table/app-table.tsx
@@ -2,7 +2,7 @@ import { Component, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'app-table',
-  styleUrl: 'app-table.css',
+  // styleUrl: 'app-table.css',
   shadow: true,
 })
 export class AppTable {
